4a591df7e3b98a6e65ec1fbe5e56c53a5d6ca7be
fmarabini93 <fmarabini93>
2021-04-30 14:21:21 +0200

    base-touch-ups

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 var names = ["Bianchi", "Rossi", "Neri", "Verdi", "Gialli"];
 var yourName = prompt("Insert your last name");
 var position;
+var nameList;
 
 names.push(yourName);
 names = names.sort();
 
 position = names.indexOf(yourName);
 
-document.getElementById("names").innerHTML = names;
+nameList = document.getElementById("names");
+for (i=0; i<6; i++) {
+      nameList.innerHTML += "<li>" + names[i] + "</li>";
+}
+
 document.getElementById("position").innerHTML = "You are in position nr. " + "<strong>" + (position + 1) + "</strong>" + " of the list";
